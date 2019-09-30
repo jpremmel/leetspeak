a0e05ee00e61240e543f38231a123ffa8fbfff4a
Jacqueline Remmel and Mike McShane <jacqueline@>
2019-09-30 09:35:27 -0700

    restructured program skeleton so it stops yelling at us.

---
+++ Leetspeak.Solution/Leetspeak.Tests/ModelTests/LeetspeakTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Translator;
+
+namespace Translator.Test
+{
+    [TestClass]
+    public class LeetspeakTest
+    {
+        [TestMethod]
+        
+    }
+}
---
+++ Leetspeak.Solution/Leetspeak/Models/Leetspeak.cs
@@ -0,0 +1,12 @@
+namespace Translator
+{
+    public class Leetspeak
+    {
+        public string Translate(string input)
+        {
+            string leetspeak = "";
+            //logic
+            return leetspeak;
+        }
+    }
+}
